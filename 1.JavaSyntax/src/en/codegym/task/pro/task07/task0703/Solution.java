52748a8d70aabe65c6d937c258e895542b5b93fb
Tom <thomas.spencer@codurance.com>
2022-05-25 07:25:46 +0100

    Add scientific notation practice

---
+++ 1.JavaSyntax/src/en/codegym/task/pro/task07/task0703/Solution.java
@@ -0,0 +1,17 @@
+package en.codegym.task.pro.task07.task0703;
+
+/* 
+Minimum integer
+*/
+
+public class Solution {
+
+  //write your code here
+  byte a = Byte.MIN_VALUE;
+  short b = Short.MIN_VALUE;
+  int c = Integer.MIN_VALUE;
+  long d = Long.MIN_VALUE;
+  float e = Float.MIN_VALUE;
+  double f = Double.MIN_VALUE;
+  char g = Character.MIN_VALUE;
+}
