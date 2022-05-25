cf0d29b9b838356ec537d5898e6c558618b25bd8
Tom <thomas.spencer@codurance.com>
2022-05-25 07:45:08 +0100

    Add "Hello char" with byte chars

---
+++ 1.JavaSyntax/src/en/codegym/task/pro/task07/task0705/Solution.java
@@ -0,0 +1,17 @@
+package en.codegym.task.pro.task07.task0705;
+
+/* 
+To infinity and beyond!
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //write your code here
+        div(0.0, 10);
+        div(0.0, -10);
+    }
+
+    public static void div(double a, double b){
+        System.out.println(b/a);
+    }
+}
---
+++ 1.JavaSyntax/src/en/codegym/task/pro/task07/task0706/Solution.java
@@ -0,0 +1,16 @@
+package en.codegym.task.pro.task07.task0706;
+
+/* 
+Strange division
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //write your code here
+        div(0.0, 0.0);
+    }
+
+    public static void div(double a, double b){
+        System.out.println(b/a);
+    }
+}
