05f2c39cbc724329af3ca2af1e9199829e778bfd
Tom <thomas.spencer@codurance.com>
2022-05-25 07:17:26 +0100

    Add max values for primitive integer types

---
+++ 1.JavaSyntax/src/en/codegym/task/pro/task07/task0702/Solution.java
@@ -0,0 +1,25 @@
+package en.codegym.task.pro.task07.task0702;
+
+/* 
+Maximum integer
+*/
+
+public class Solution {
+    //write your code here
+//  byte	1	-128 .. 127	0	The smallest integer type is a single byte
+//  short	2	-32,768 .. 32.767	0	Short integer, two bytes
+//  int	4	-2*109 .. 2*109	0	Integer, 4 bytes
+//  long	8	-9*1018 .. 9*1018	0L	Long integer, 8 bytes
+//  float	4	-1038 .. 1038	0.0f	Floating-point number, 4 bytes
+//  double	8	-10308 .. 10308	0.0d	Double-precision floating point number, 8 bytes
+//  boolean	1	true, false	false	Boolean type (only true and false)
+//  char	2	0 .. 65.535	'\u0000'	Characters, 2 bytes, all greater than 0
+
+  byte a = Byte.MAX_VALUE;
+  short b = Short.MAX_VALUE;
+  int c = Integer.MAX_VALUE;
+  long d = Long.MAX_VALUE;
+  float e = Float.MAX_VALUE;
+  double f = Double.MAX_VALUE;
+  char g = Character.MAX_VALUE;
+}
