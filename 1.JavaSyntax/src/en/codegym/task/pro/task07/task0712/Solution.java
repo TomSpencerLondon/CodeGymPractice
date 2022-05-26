4a6770a3688cca979b94871c106861d6ed9c4e5a
Tom <thomas.spencer@codurance.com>
2022-05-26 19:41:43 +0100

    Add object creation

---
+++ 1.JavaSyntax/src/en/codegym/task/pro/task07/task0712/Solution.java
@@ -0,0 +1,31 @@
+package en.codegym.task.pro.task07.task0712;
+
+import java.util.Scanner;
+
+/* 
+First objects
+*/
+
+public class Solution {
+
+  public static void main(String[] args) {
+    //write your code here
+    String string1 = new String();
+    String string2 = new String();
+    String string3 = new String();
+    String string4 = new String();
+    String string5 = new String();
+    String string6 = new String();
+    String string7 = new String();
+    String string8 = new String();
+    String string9 = new String();
+    String string10 = new String();
+    int[] array1 = new int[]{};
+    int[] array2 = new int[]{};
+    int[] array3 = new int[]{};
+    int[] array4 = new int[]{};
+    int[] array5 = new int[]{};
+    Scanner scanner1 = new Scanner("");
+    Scanner scanner2 = new Scanner("");
+  }
+}
