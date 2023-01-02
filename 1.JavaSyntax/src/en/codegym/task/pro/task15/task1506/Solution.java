ef70d965ac2287c0c69466d7567268481ede2dd8
Tom Spencer <thomas.spencer@codurance.com>
2023-01-02 12:02:29 +0000

    add code gym practice

---
+++ 1.JavaSyntax/src/en/codegym/task/pro/task15/task1506/Solution.java
@@ -0,0 +1,17 @@
+package en.codegym.task.pro.task15.task1506;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Scanner;
+
+/* 
+Face control
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //write your code here
+    }
+}
+
